ddf8405fcb1ba5551b5d48f2f282ee1128aa4e69
Grazee <lkw91110@gmail.com>
2019-05-25 15:43:29 +0800

    配置makefile模式。

--- lisp/init_cpp.el
+++ lisp/init_cpp.el
@@ -1,11 +1,19 @@
 ;; (require 'package)
 
-;; tab缩进为4个
-(setq-default c-default-style "linux"
-              ;; tab 改为插入空格
-              indent-tabs-mode nil
-              ;; c c++ 缩进4个空格
-              c-basic-offset 4
-              default-tab-width 4)
+;; 配置C++模式缩进
+(add-hook
+ 'c++-mode
+ (setq-default c-default-style "linux"
+               ;; tab 改为插入空格
+               indent-tabs-mode nil
+               ;; c c++ 缩进4个空格
+               c-basic-offset 4
+               default-tab-width 4))
+
+;; 配置makefile模式缩进
+(add-hook
+ 'makefile-mode
+ (setq-default default-tab-width 4
+               tab-width 4))
 
 (provide 'init_cpp)
