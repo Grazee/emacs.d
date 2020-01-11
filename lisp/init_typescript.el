d9f32cd5d14c31c44ac9b6a15947f6c99282de41
喂草。 <lkw91110@gmail.com>
2020-01-11 11:09:13 +0800

    增加typescript下的括号补全、补全大小写不敏感。

--- lisp/init_typescript.el
+++ lisp/init_typescript.el
@@ -9,10 +9,13 @@
   :ensure t
   :config
   (progn
-    (setq tide-project-root typescript-project-root)
+    (setq
+     tide-project-root typescript-project-root
+     tide-completion-ignore-case t)
     (tide-start-server)
     (tide-setup))
   :hook
-  (typescript-mode . tide-mode))
+  (typescript-mode . tide-mode)
+  (typescript-mode . electric-pair-mode))
 
 (provide 'init_typescript)
