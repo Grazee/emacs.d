c047c46ebd8777ee9e2ef82e12ceeb91d63a8b7e
Grazee <lkw91110@gmail.com>
2021-04-04 13:18:25 +0800

    注释掉ECB环境。。没啥用还会每次打开报警。

--- init.el
+++ init.el
@@ -27,7 +27,7 @@
 (require 'init_lisp)
 (require 'init_cpp)
 ;; (require 'init_java)
-(require 'init_go)
+;; (require 'init_go)
 ;; (require 'init_typescript)
 ;; (require 'init_csharp)
 (require 'init_web)
