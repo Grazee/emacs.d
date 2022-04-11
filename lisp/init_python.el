67eb5ef780d97bab043b8d52f2768df38447b823
Grazee <lkw91110@gmail.com>
2022-04-11 09:43:30 +0800

    python环境使用lsp。

--- lisp/init_python.el
+++ lisp/init_python.el
@@ -3,7 +3,7 @@
 (if (eq system-type 'darwin)
     ;; MacOS的python3目录
     ;; (setq python3Dic "/usr/local/bin")
-  (setq python3Dic "/usr/local/opt/python@3.8/bin")
+  (setq python3Dic "/usr/local/bin")
   ;; 其他Linux系统的python3目录
   (setq python3Dic "/usr/bin"))
 
@@ -23,33 +23,47 @@
 (add-hook 'python-mode-hook 'my-python-mode-config)
 
 ;;启用elpy的python开发环境，包括创建python虚拟环境
-(use-package elpy
-  :defer t
-  :ensure t
-  :config
-  (setq
-   ;; 使用python3
-   elpy-rpc-python-command "python3")
-  :init
-  (advice-add 'python-mode :before 'elpy-enable))
+;; (use-package elpy
+;;   :defer t
+;;   :ensure t
+;;   :config
+;;   (setq
+;;    ;; 使用python3
+;;    elpy-rpc-python-command "python3")
+;;   :init
+;;   (advice-add 'python-mode :before 'elpy-enable))
 
 ;; python自动补全工具
-(use-package company-jedi
-  :defer t
-  :ensure t
-  :init
-  (progn
-    (setq jedi:get-in-function-call-delay 0)
-    (add-hook 'python-mode-hook 'jedi:setup)
-    ;; 在打出点的时候弹出补全
-    (setq jedi:complete-on-dot t)
-    ;; 补全的时候识别简写
-    (setq jedi:use-shortcuts t)
-    ;; 补全列表循环
-    (setq company-selection-wrap-around t)
-    ;; 虚拟环境，使用elpy的虚拟环境
-    (setq jedi:environment-root "../elpy/rpc-venv")))
+;; (use-package company-jedi
+;;   :defer t
+;;   :ensure t
+;;   :init
+;;   (progn
+;;     (setq jedi:get-in-function-call-delay 0)
+;;     (add-hook 'python-mode-hook 'jedi:setup)
+;;     ;; 在打出点的时候弹出补全
+;;     (setq jedi:complete-on-dot t)
+;;     ;; 补全的时候识别简写
+;;     (setq jedi:use-shortcuts t)
+;;     ;; 补全列表循环
+;;     (setq company-selection-wrap-around t)
+;;     ;; 虚拟环境，使用elpy的虚拟环境
+;;     (setq jedi:environment-root "../elpy/rpc-venv")))
 
+(use-package lsp-mode
+  :ensure t
+  :config
+  (lsp-register-custom-settings
+   '(("pyls.plugins.pyls_mypy.enabled" t t)
+     ("pyls.plugins.pyls_mypy.live_mode" nil t)
+     ("pyls.plugins.pyls_black.enabled" t t)
+     ("pyls.plugins.pyls_isort.enabled" t t)))
+  :hook
+  ((python-mode . lsp-mode)))
+
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
 
 (provide 'init_python)
 ;;; init_python ends here
